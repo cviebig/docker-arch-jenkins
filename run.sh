ccf0a462d55db383cab5aa619e784b0f2e446b0d
Christoph Viebig <git@christoph-viebig.de>
2015-09-10 14:38:54 +0200

    Wipe Jenkins SSH configuration on (re)start

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 export PORT=4243
 wrapdocker &
+rm -rf /var/lib/jenkins/.ssh
 if [ -d "/root/config-ssh" ]; then
     cp -R /root/config-ssh /var/lib/jenkins/.ssh
     chown -R jenkins:jenkins /var/lib/jenkins/.ssh
