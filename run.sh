3e3e1cbc90bd922d91aeda220821b3023414a409
Christoph Viebig <git@christoph-viebig.de>
2015-09-10 14:11:46 +0200

    Import SSH configuration for Jenkins if available

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 export PORT=4243
 wrapdocker &
+if [ -d "/root/config-ssh" ]; then
+    cp -R /root/config-ssh /var/lib/jenkins/.ssh
+    chown -R jenkins:jenkins /var/lib/jenkins/.ssh
+fi
 chown -R jenkins:jenkins /var/lib/jenkins/.jenkins/jobs
 chmod -R 750 /var/lib/jenkins/.jenkins/jobs
 su -c "sh updatejobs.sh" - jenkins
