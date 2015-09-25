4fb88cbe35da8b72ddeb25fe80a8db3a068a493e
Christoph Viebig <git@christoph-viebig.de>
2015-09-25 14:32:08 +0200

    Change job directory ownership after mkdir

--- run.sh
+++ run.sh
@@ -6,7 +6,5 @@ if [ -d "/root/config-ssh" ]; then
     cp -R /root/config-ssh /var/lib/jenkins/.ssh
     chown -R jenkins:jenkins /var/lib/jenkins/.ssh
 fi
-chown -R jenkins:jenkins /var/lib/jenkins/.jenkins/jobs
-chmod -R 750 /var/lib/jenkins/.jenkins/jobs
 su -c "sh updatejobs.sh" - jenkins
 su -c "java -jar /usr/share/java/jenkins/jenkins.war \"$@\"" - jenkins
--- updatejobs.sh
+++ updatejobs.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 mkdir -v -p /var/lib/jenkins/.jenkins/jobs
+chown -R jenkins:jenkins /var/lib/jenkins/.jenkins/jobs
+chmod -R 750 /var/lib/jenkins/.jenkins/jobs
 cd /var/lib/jenkins/.jenkins/jobs
 git init
 git remote add origin https://github.com/cviebig/jenkins-jobs.git || true
